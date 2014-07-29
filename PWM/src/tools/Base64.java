7c3e30e780ddb9956c897dce8e1cc8cb72de56bb
tobias.sdun <tobias.sdun@googlemail.com>
2014-07-29 14:09:31 +0200

    sha and base64 stuff

--- PWM/src/tools/Base64.java
+++ PWM/src/tools/Base64.java
@@ -1,8 +1,13 @@
 package tools;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Base64 {
 	public static void main(String[] args){
 		System.out.println(encode("Hallo"));
+		System.out.println(decode("SGFsbG8="));
+		System.out.println(SHA.getHash("Hallo", "SHA-512", true));
 	}
 
 	public static String encode(String mes){
@@ -12,25 +17,66 @@ public class Base64 {
 			//convert char to byte, ex: o=111 in ascii
 			bytes[i] = (byte)mes.charAt(i);
 		}
-		String str = "";
+		boolean[][] bits = new boolean[bytes.length+bytes.length%3][8];
 		for(int j = 0; j< bytes.length ; j++){
-			boolean isR = false;
-			while(!isR){
-				if(bytes[j]%2 == 0) str+="0";
-				else str+="1";
+			int w = 7;
+			while(w >= 0){
+				if(bytes[j]%2 == 0) bits[j][w] = false;else bits[j][w] = true;
 				bytes[j] = (byte) (bytes[j]/2);
-				if(bytes[j] == 0) isR = true;
+				//System.out.println(bits[j][w]);
+				w--;
+			}
+		}
+		int addedZeros = 0;
+		if(bytes.length % 3 != 0){
+			if(bytes.length % 3 == 1){
+				for(int n = 0; n<8; n++){
+					bits[bytes.length+1][n] = false;
+					bits[bytes.length+2][n] = false;
+					addedZeros = 2;
+				}
+			}else if(bytes.length % 3 == 2){
+				for(int n = 0; n<8; n++){
+					bits[bytes.length+1][n] = false;
+					addedZeros = 1;
+				}
 			}
 		}
 		String st = "";
-		for(int m = 0; m<str.length();m++){
-			if(m%6==0 && m!=0) st += "|";
-			st += str.charAt(m);
+		for(int i = 0; i<bits.length ; i++){
+			for(int j = 0; j<bits[0].length; j++){
+				if(bits[i][j]) st+="1"; else st+="0";
+			}
 		}
-		return st;
+		//System.out.println(st);
+		String out = "";
+		for(int i = 0; i < st.length()/6-addedZeros-1;i++){
+			int m = 0;
+			for(int j = 0; j < 6; j++){
+				if(st.charAt((i*6)+j) == '1'){
+					m += Math.pow(2, 5-j);
+					//System.out.println("m: " +((i*6)+j));
+				}
+			}
+			//System.out.println(m);
+			out += b64[m];
+		}
+		for(int i = 0; i < addedZeros; i++){
+			out += "=";
+		}
+		return out;
 	}
 	
-	private static boolean isSet(byte value, int bit){
-		   return (value&(1<<bit))!=0;
-	} 
+	public static String decode(String mes){
+		String[] b64 = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/", "="};
+		int[] sixBytes = new int[mes.length()];
+		for(int i = 0; i<mes.length(); i++){
+			for(int j = 0; j<b64.length; j++){
+				if(mes.charAt(i) == b64[j].charAt(0)) sixBytes[i] = j;
+			}
+		}
+		
+		return "";
+		
+	}
 }
--- PWM/src/tools/SHA.java
+++ PWM/src/tools/SHA.java
@@ -1,4 +1,49 @@
 package tools;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Tobias Sdun
+ * @version 1.0
+ */
 public class SHA {
+	
+	/** 
+     * Calculate the a hash of a given String
+     *
+     * @param mes the String to hash
+     * @param alg what algorithm should be used for hashing (SHA-512,MD5,...)
+     * @param hexHash should give it back a hex string?
+     * 
+     * @return get back a String as hash
+     */
+	public static String getHash(String mes, String alg, boolean hexHash) {
+		try {
+			MessageDigest md = MessageDigest.getInstance(alg);
+			md.update(mes.getBytes());
+			if(hexHash){
+				return hexHash(md.digest());
+			}else{
+				return new String(md.digest());
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static String hexHash(byte[] mb) {
+		String out = "";
+		for (int i = 0; i < mb.length; i++) {
+			byte temp = mb[i];
+			String s = Integer.toHexString(new Byte(temp));
+			while (s.length() < 2) {
+				s = "0" + s;
+			}
+			s = s.substring(s.length() - 2);
+			out += s;
+		}
+		return out;
+	}
 }
