c4ee2298c30cbcc00a95c16abd00e21eb85431aa
tobias.sdun <tobias.sdun@googlemail.com>
2014-07-29 21:24:15 +0200

    delete some stuff

--- PWM/src/tools/TESTS.java
+++ PWM/src/tools/TESTS.java
@@ -1,19 +1,10 @@
 package tools;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 import tools.SHA.TypeToGiveBack;
 
 public class TESTS {
 
 	public static void main(String[] args) {
-		System.out.println(MyBase64.encode("Hallo"));
-		System.out.println(MyBase64.decode("SGFsbG8="));
 		System.out.println(SHA.getHash("Hallo", "SHA-512", TypeToGiveBack.HEXSTRING));
 		String en = AES.encode("aaaaa", "Hello WOrld")+ "|" + AES.encode("aaaaa", "Hello W") + "|" + AES.encode("aaaaa", "Hello");
 		en = AES.encode("aaaaa", en);
