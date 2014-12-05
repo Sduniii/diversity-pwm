511380ac8880ef8b3068bdade55328cd7699fa20
Sduniii <tobias.sdun@googlemail.com>
2014-12-05 21:38:35 +0100

    add filechooser! create user doesnt work anymore!

--- PWM/src/gui/LoginForm.java
+++ PWM/src/gui/LoginForm.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -14,10 +15,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Base64;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.jdesktop.swingx.JXButton;
 import org.jdesktop.swingx.JXFrame;
@@ -26,11 +31,12 @@ import org.jdesktop.swingx.JXLoginPane;
 
 import tools.SHA;
 import tools.SHA.TypeToGiveBack;
-import java.awt.Toolkit;
 
 public class LoginForm {
 	private JXFrame frame;
 	JXLoginPane loginPane;
+	private JFileChooser fc = new JFileChooser();
+	File fileFC = null;
 
 	/**
 	 * Launch the application.
@@ -77,6 +83,27 @@ public class LoginForm {
 
 		loginPane = new JXLoginPane();
 		loginPane.setBounds(10, 11, 414, 164);
+		JPanel ttt = ((JPanel)((JPanel)((JPanel)((JPanel)loginPane.getComponent(1)).getComponent(0)).getComponent(1)).getComponent(1));
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "*.ts", "ts");
+		fc.setFileFilter(filter);
+		fc.setDialogTitle("Passwort Datei auswählen");
+		JXButton btnFC = new JXButton();
+		btnFC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			        int returnVal = fc.showOpenDialog(frame);
+
+			        if (returnVal == JFileChooser.APPROVE_OPTION) {
+			            fileFC = fc.getSelectedFile();
+			            ((JXButton)ttt.getComponent(0)).setText(fileFC.getAbsolutePath());
+			            //This is where a real application would open the file.
+			        } 
+			}
+		});
+		btnFC.setText("...");
+		((JLabel)ttt.getParent().getComponent(0)).setText("Datei");;
+		ttt.remove(ttt.getComponent(0));
+		ttt.add(btnFC,0);
 		frame.getRootPaneExt().getContentPane().add(loginPane);
 
 		JXButton btnOk = new JXButton();
@@ -111,18 +138,18 @@ public class LoginForm {
 
 	private void createActions() {
 		try {
-			System.out.println(System.getProperty("user.dir")+System.getProperty("file.separator") + loginPane.getUserName() + ".ts");
-			File file = new File(System.getProperty("user.dir")+System.getProperty("file.separator") + loginPane.getUserName() + ".ts");
+			//System.out.println(System.getProperty("user.dir")+System.getProperty("file.separator") + loginPane.getUserName() + ".ts");
+			if(fileFC != null){
 			if (loginPane.getBannerText().trim().equals("Anmeldung")) {
-				if (file.exists()) {
+				if (fileFC.exists()) {
 					@SuppressWarnings("resource")
-					BufferedReader fr = new BufferedReader(new FileReader(file));
+					BufferedReader fr = new BufferedReader(new FileReader(fileFC));
 					String pa = fr.readLine();
 					String sss = (String)SHA.getHash(new String(loginPane.getPassword()), "Sha-512", TypeToGiveBack.HEXSTRING);
 					sss = (String)SHA.getHash(sss, "Sha-512", TypeToGiveBack.HEXSTRING);
 					if(new String(Base64.getUrlDecoder().decode(pa)).equals(sss)){
 						frame.dispose();
-						new SplashScreen(new String(loginPane.getPassword()),file);
+						new SplashScreen(new String(loginPane.getPassword()),fileFC);
 					}else{
 						JOptionPane.showMessageDialog(frame,
 								"falsches Passwort!", "Fehler",
@@ -134,15 +161,15 @@ public class LoginForm {
 							JOptionPane.ERROR_MESSAGE);
 				}
 			} else {
-				if (file.exists()) {
+				if (fileFC.exists()) {
 					JOptionPane.showMessageDialog(frame,
 							"Benutzer existiert bereits!", "Fehler",
 							JOptionPane.ERROR_MESSAGE);
 				} else {
 					if (loginPane.getUserName().length() > 0 && loginPane.getPassword().length > 4) {
 
-						file.createNewFile();
-						BufferedWriter fw = new BufferedWriter(new FileWriter(file));
+						fileFC.createNewFile();
+						BufferedWriter fw = new BufferedWriter(new FileWriter(fileFC));
 						String sss = (String)SHA.getHash(new String(loginPane.getPassword()), "Sha-512", TypeToGiveBack.HEXSTRING);
 						sss = (String)SHA.getHash(sss, "Sha-512", TypeToGiveBack.HEXSTRING);
 						fw.write(new String(Base64.getUrlEncoder().encodeToString(sss.getBytes())));
@@ -158,6 +185,9 @@ public class LoginForm {
 					}
 
 				}
+			}
+			}else{
+				JOptionPane.showMessageDialog(null, "Datei existiert nicht!", "Fehler", JOptionPane.ERROR_MESSAGE);
 			}
 		} catch (IOException e) {
 			
