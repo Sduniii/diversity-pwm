7dca8cb31748cf5ccc5989098b0cb2ac644fabf3
Sduniii <tobias.sdun@googlemail.com>
2014-12-06 13:47:52 +0100

    same as before

---
+++ PWM/src/models/MyTableModel.java
@@ -0,0 +1,25 @@
+package models;
+
+import java.util.Vector;
+
+import javax.swing.table.DefaultTableModel;
+
+public class MyTableModel extends DefaultTableModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8407048378543690551L;
+
+	public MyTableModel(Object[][] objects, String[] strings) {
+		super(objects,strings);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void setValueAt(Object value, int row, int column) {
+		if (!getValueAt(row, column).equals(value)) {
+			((Vector) dataVector.get(row)).set(column, value);
+			fireTableCellUpdated(row, column);
+		}
+	}
+}
