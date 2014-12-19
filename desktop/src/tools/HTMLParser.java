d6787d85f72c9561b1bb299056593bc2abdabef4
Sduniii <tobias.sdun@gmail.com>
2014-12-19 15:02:28 +0100

    html

--- desktop/src/tools/HTMLParser.java
+++ desktop/src/tools/HTMLParser.java
@@ -1,34 +1,89 @@
 package tools;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.LinkedList;
 
-public class HTMLParser {
+import javax.swing.SwingWorker;
+
+public class HTMLParser extends SwingWorker<Void, Void>{
+
+	File file;
+	LinkedList<String> list;
 	
-	public static LinkedList<String> parse(String urlS){
+	public HTMLParser(File file, LinkedList<String> list) {
+		this.file = file;
+		this.list = list;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+	public Void doInBackground() {
+		try {
+
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String str;
+			boolean found = false;
+
+			while ((str = in.readLine()) != null) {
+				if (str.contains("id=\"tbl-pwm\"")) {
+					found = true;
+				}
+
+				if (found && str.contains("</table>")) {
+					found = false;
+				}
+
+				if (str.contains("<td>") && found) {
+					String tS = str.substring(str.indexOf("<td>") + 4,
+							str.indexOf("</td>"));
+					if (!tS.contains("<b>")) {
+						list.add(str.substring(str.indexOf("<td>") + 4,
+								str.indexOf("</td>")));
+					}
+				}
+
+			}
+			in.close();
+			return null;
+		}catch(Exception ex){
+			return null;
+		}
+	}
+
+	public LinkedList<String> parse(File urlS) {
 		LinkedList<String> list = new LinkedList<String>();
 		try {
-		    // Create a URL for the desired page
-		    URL url = new URL("file:///" + urlS);
-		    
-		    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-		    String str;
 
-		    while ((str = in.readLine()) != null) {
-		    	if(str.contains("<td>")){
-		    		String tS = str.substring(str.indexOf("<td>")+4, str.indexOf("</td>"));
-		    		if(!tS.contains("<b>")){
-		    			list.add(str.substring(str.indexOf("<td>")+4, str.indexOf("</td>")));
-		    		}
-		    	}
+			BufferedReader in = new BufferedReader(new FileReader(urlS));
+			String str;
+			boolean found = false;
 
-		    }
-		    in.close();
-		    return list;
+			while ((str = in.readLine()) != null) {
+				if (str.contains("id=\"tbl-pwm\"")) {
+					found = true;
+				}
+
+				if (found && str.contains("</table>")) {
+					found = false;
+				}
+
+				if (str.contains("<td>") && found) {
+					String tS = str.substring(str.indexOf("<td>") + 4,
+							str.indexOf("</td>"));
+					if (!tS.contains("<b>")) {
+						list.add(str.substring(str.indexOf("<td>") + 4,
+								str.indexOf("</td>")));
+					}
+				}
+
+			}
+			in.close();
+			return list;
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 			return list;
